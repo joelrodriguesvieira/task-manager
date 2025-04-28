c7084d7088e9349fe31c854a91341658f08c69dd
joelrodriguesvieira <joel2j39@gmail.com>
2025-04-28 12:36:39 -0300

    fix(api): change from three environment variables to unique environment variable and change connecting from mongodb to mongoose

--- apps/api/src/config/database.ts
+++ apps/api/src/config/database.ts
@@ -1,20 +1,12 @@
-import { MongoClient as Mongo, Db } from "mongodb";
+import mongoose from "mongoose";
 
-export const MongoClient = {
-  client: undefined as unknown as Mongo,
-  db: undefined as unknown as Db,
-
-  async connect(): Promise<void> {
-    const url = process.env.MONGODB_URL || "localhost:2701";
-    const username = process.env.MONGODB_USERNAME;
-    const password = process.env.MONGODB_PASSWORD;
-
-    const client = new Mongo(url, { auth: { username, password } });
-    const db = client.db("task-manager");
-
-    this.client = client;
-    this.db = db;
-
-    console.log('Connected to Mongodb Atlas')
-  },
+export async function connectDatabase(): Promise<void> {
+  try {
+    const url = process.env.MONGODB_URL || "mongodb://localhost:27017/task-manager";
+    await mongoose.connect(url);
+    console.log("Connected to MongoDB Atlas (via Mongoose)");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
 };
