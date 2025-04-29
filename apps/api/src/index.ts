8cbb9f29b2ad78714b1a9adeff9d7a495b9d0790
joelrodriguesvieira <joel2j39@gmail.com>
2025-04-29 08:55:39 -0300

    feature(api): handle cors in index.js

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { config } from "dotenv";
 import { connectDatabase } from "./config/database.js";
 import { taskRoutes } from "./routes/tasks/task.routes.js";
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/tasks", taskRoutes);
 
